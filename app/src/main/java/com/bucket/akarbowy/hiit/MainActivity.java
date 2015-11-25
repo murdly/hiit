b5619fa89f3515fea87ddbf1c199ef6b3dde73af
Arkadiusz Karbowy <arkadiusz.karbowy@gmail.com>
2015-11-25 19:31:36 +0100

    init

---
+++ app/src/main/java/com/bucket/akarbowy/hiit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bucket.akarbowy.hiit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

911d6a6e9088d8c3d1c03c7e1e9dfacf7c7f085f
Arkadiusz Karbowy <arkadiusz.karbowy@gmail.com>
2015-11-26 19:32:36 +0100

    setup tabs with fragment placeholders, define theme colors

---
+++ app/src/main/java/com/bucket/akarbowy/hiit/view/activities/MainActivity.java
@@ -0,0 +1,82 @@
+package com.bucket.akarbowy.hiit.view.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.bucket.akarbowy.hiit.R;
+import com.bucket.akarbowy.hiit.view.adapters.MyPagerAdapter;
+import com.bucket.akarbowy.hiit.view.enums.Tab;
+import com.bucket.akarbowy.hiit.view.fragments.TabFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
+    @Bind(R.id.tabLayout)
+    TabLayout mTabLayout;
+    @Bind(R.id.fab)
+    FloatingActionButton mFab;
+
+    private TabFragment mRssFragment, mParticipateFragment, mAccountFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initViewPagerAndTabs();
+        setupTabsIcons();
+
+        mToolbar.setTitle(Tab.getTitle(this, mTabLayout.getSelectedTabPosition()));
+        setSupportActionBar(mToolbar);
+    }
+
+    private void initViewPagerAndTabs() {
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mRssFragment = TabFragment.createInstance(Tab.RSS);
+        mParticipateFragment = TabFragment.createInstance(Tab.PARTICIPATE);
+        mAccountFragment = TabFragment.createInstance(Tab.ACCOUNT);
+
+        pagerAdapter.addFragment(mRssFragment, null);
+        pagerAdapter.addFragment(mParticipateFragment, null);
+        pagerAdapter.addFragment(mAccountFragment, null);
+        mViewPager.setAdapter(pagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.setOnTabSelectedListener(mOnTabSelectedListener);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void setupTabsIcons() {
+        for(int i = 0; i< mTabLayout.getTabCount(); i++)
+            mTabLayout.getTabAt(i).setIcon(Tab.getIcon(this, i));
+    }
+
+    private TabLayout.OnTabSelectedListener mOnTabSelectedListener = new TabLayout.OnTabSelectedListener(){
+
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+            mViewPager.setCurrentItem(tab.getPosition());
+            mToolbar.setTitle(Tab.getTitle(getApplicationContext(), tab.getPosition())); //todo get rid of context
+        }
+
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/bucket/akarbowy/hiit/view/enums/Tab.java
@@ -0,0 +1,50 @@
+package com.bucket.akarbowy.hiit.view.enums;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+
+import com.bucket.akarbowy.hiit.R;
+
+/**
+ * Created by akarbowy on 26.11.2015.
+ */
+public enum Tab {
+    RSS (0, R.drawable.ic_rss, R.string.tab_rss),
+    PARTICIPATE(1, R.drawable.ic_participate, R.string.tab_participate),
+    ACCOUNT(2, R.drawable.ic_account, R.string.tab_account);
+
+    private int mPosition;
+    private int mDrawableId;
+    private int mTitleId;
+
+    Tab(int position, int drawableId, int titleId) {
+        this.mPosition = position;
+        this.mDrawableId = drawableId;
+        this.mTitleId = titleId;
+    }
+
+    public int getPosition(){
+        return mPosition;
+    }
+
+    public static Drawable getIcon(Context context, int position){
+        int resId = RSS.mDrawableId;
+        if(PARTICIPATE.mPosition == position)
+           resId = PARTICIPATE.mDrawableId;
+        else if(ACCOUNT.mPosition == position)
+           resId = ACCOUNT.mDrawableId;
+
+      return ContextCompat.getDrawable(context, resId);
+    }
+
+    public static String getTitle(Context context, int position){
+        int resId = RSS.mTitleId;
+        if(PARTICIPATE.mPosition == position)
+            resId = PARTICIPATE.mTitleId;
+        else if(ACCOUNT.mPosition == position)
+            resId = ACCOUNT.mTitleId;
+
+        return context.getString(resId);
+    }
+}
